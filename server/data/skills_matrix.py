9c28adad3d9fde78eb46eca525f646a7db503212
Mohamed Amine Yahia <mohamedamineyahia03@gmail.com>
2026-08-30 00:13:58 +0100

    Ajout matrice compétences SmartProf

---
+++ server/data/skills_matrix.py
@@ -0,0 +1,37 @@
+SKILLS_MATRIX = {
+    "1": {
+        "math": {
+            "T1": ["Nombres", "Addition", "Soustraction", "Comparaison", "Problèmes"],
+            "T2": ["Nombres", "Addition", "Soustraction", "Mesures", "Problèmes"],
+            "T3": ["Nombres", "Calcul", "Géométrie", "Mesures", "Problèmes"],
+        }
+    },
+    "2": {
+        "math": {
+            "T1": ["Nombres", "Addition", "Soustraction", "Multiplication", "Problèmes"],
+            "T2": ["Nombres", "Multiplication", "Division", "Mesures", "Problèmes"],
+            "T3": ["Calcul", "Géométrie", "Mesures", "Problèmes"],
+        }
+    },
+    "3": {
+        "math": {
+            "T1": ["Nombres", "Calcul", "Multiplication", "Division", "Problèmes"],
+            "T2": ["Fractions", "Mesures", "Géométrie", "Problèmes"],
+            "T3": ["Calcul", "Géométrie", "Mesures", "Problèmes"],
+        }
+    },
+    "4": {
+        "math": {
+            "T1": ["Nombres", "Calcul", "Fractions", "Problèmes"],
+            "T2": ["Fractions", "Mesures", "Géométrie", "Problèmes"],
+            "T3": ["Calcul", "Géométrie", "Mesures", "Problèmes"],
+        }
+    },
+    "5": {
+        "math": {
+            "T1": ["Nombres", "Calcul", "Fractions", "Problèmes"],
+            "T2": ["Fractions", "Décimaux", "Mesures", "Géométrie", "Problèmes"],
+            "T3": ["Calcul", "Géométrie", "Mesures", "Problèmes"],
+        }
+    },
+}
